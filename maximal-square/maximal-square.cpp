9fe81efec64fcea82a5843cc0aba429c496fc17e
Pankaj Ku Chouhan <43536789+pkc189@users.noreply.github.com>
2021-08-10 23:32:37 +0530

    Time: 24 ms (70.44%), Space: 11.9 MB (41.07%) - LeetHub

--- maximal-square/maximal-square.cpp
+++ maximal-square/maximal-square.cpp
@@ -1,21 +1,38 @@
 class Solution {
 public:
-    int maximalSquare(vector<vector<char>>& matrix) {
-        if (matrix.empty()) {
+    int maximalSquare(vector<vector<char>>& m) {
+        int row = m.size();
+        int col = m[0].size();
+        
+        if (m.empty()) {
             return 0;
         }
-        int m = matrix.size(), n = matrix[0].size(), sz = 0;
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (!i || !j || matrix[i][j] == '0') {
-                    dp[i][j] = matrix[i][j] - '0';
-                } else {
-                    dp[i][j] = min(dp[i - 1][j - 1], min(dp[i - 1][j], dp[i][j - 1])) + 1;
+        
+      vector<vector<int>>dp(row,vector<int>(col,0));
+        int sz=0;
+   
+        
+        
+        for(int i =0;i<m.size();i++)
+        {
+            for(int j=0;j<m[i].size();j++)
+            {
+                if(!i || !j || m[i][j]=='0')
+                {
+                   dp[i][j] = m[i][j]-'0';
                 }
-                sz = max(dp[i][j], sz);
+                else
+                {
+                   dp[i][j] = min(dp[i - 1][j - 1], min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                }
+                
+                sz = max(sz,dp[i][j]);
             }
         }
-        return sz * sz;
+        
+        
+        return sz*sz;
+        
+        
     }
 };
